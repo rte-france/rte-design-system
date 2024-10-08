d39178d1ee523186fc8d43e02fb5bef7bf4f4c25
capyq <quentin.capy@rte-france.com>
2024-10-08 15:34:37 +0200

    chore:init angular project
    Signed-off-by: capyq <quentin.capy@rte-france.com>

---
+++ packages/we-love-ds-angular/src/app/app.component.ts
@@ -0,0 +1,19 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'we-love-ds-angular';
+}
